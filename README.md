484484614d7ff8c078c799b223c2b8fdde3fc36c
waters <453539897@qq.com>
2021-06-21 20:02:55 +0800

    添加 readme, 以及MySQL

---
+++ README.md
@@ -0,0 +1,42 @@
+# ssm-framework
+
+
+## SSM框架整合
+
+### 环境(environment)：
+
+JDK-1.8 + Tomcat-9 + Maven-3.8.1+ MySQL-8
+
+### 框架 (framework)：
+
+Spring-5 + SpringMVC-5 + Mybatis-3 + c3p0 + bookstrap(前端)
+
+
+
+
+
+## 使用
+
+git clone https://github.com/waterszhao/ssm-framework.git
+
+git clone https://gitee.com/tax_payable/ssm-framework.git
+
+配置Tomcat服务器即可使用
+
+
+
+## 简介
+
+### Version 1:
+
+完成SSM大致框架，并添加了书本库类 功能包括增删改查
+
+
+
+### Version 2：
+
+添加管理系统，包括管理员与普通用户的注册等功能
+
+~~添加了主页以及一些注释~~
+
+添加MySQL语句
---
+++ mysql/ssmstudy_books.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ssmstudy
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `bookID` int(11) NOT NULL AUTO_INCREMENT,
+  `bookName` varchar(45) DEFAULT NULL,
+  `bookCount` int(11) DEFAULT NULL,
+  `detail` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`bookID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'C++',10,'C++天下第一！'),(2,'Java',30,'Java天下第一'),(3,'Python',20,'Python天下...'),(4,'JSP - Java Servlet Page',20,'JSP，狗都不学（真香！）'),(8,'PHP',20,'PHP是最好的语言！');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-21 20:00:34
